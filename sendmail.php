f00dd5b52d0e1306067faffe47ddc3ffabbad32a
KostyaO <#^ZUxtwp86k;+^>
2022-02-10 13:23:08 +0200

    edit scripts  & add php

---
+++ sendmail.php
@@ -0,0 +1,27 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->IsHTML(true);
+
+    $mail->setForm()
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
